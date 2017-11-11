f317cbed79b3fc7fe9987f2a423686d049dfae43
davidemiani <davide.miani2@gmail.com>
2017-11-11 08:33:27 +0100

    Modificata la logica delle figure.

--- Exls3_class/Exls3.m
+++ Exls3_class/Exls3.m
@@ -6,8 +6,8 @@ classdef Exls3
     %   Segment                 - description.
     %   PacketType              - description.
     %   SamplingFrequency       - description.
-    %   ShowLine                - description.
-    %   AnimatedLineHandle      - description.
+    %   ShowFigure              - description.
+    %   FigureHandle            - description.
     %   StopFcn                 - description.
     %   StartFcn                - description.
     %   SamplingFcn             - description.
@@ -19,7 +19,6 @@ classdef Exls3
     %   PacketsLostIndexes      - description.
     %   Timer                   - description.
     %   Bluetooth               - description.
-    %   CreateNewLine           - description.
     %
     % Exls3 methods:
     % Exls3 object construction:
@@ -30,8 +29,9 @@ classdef Exls3
     %   set              - Set value of Exls3 object property.
     %
     % General:
-    %
+    % ... da completare
     % Execution:
+    % ... da completare
     
     % ------------------------------------
     % CREDITS:     Davide Miani (nov 2017)
@@ -47,17 +47,17 @@ classdef Exls3
     %   * gettabili: anche dall'esterno della classe
     %   * settabili: anche dall'esterno della classe
     properties (SetAccess = public)
-        Name               = '';
-        Segment            = '';
-        PacketType         = 'A';
-        SamplingFrequency  = 50;
+        Name              = '';
+        Segment           = '';
+        PacketType        = 'A';
+        SamplingFrequency = 50;
         
-        ShowLine           = true;
-        AnimatedLineHandle = [];
+        FigureHandle      = [];
+        FigureVisible     = 'on';
         
-        StopFcn            = [];
-        StartFcn           = [];
-        SamplingFcn        = [];
+        StopFcn           = [];
+        StartFcn          = [];
+        SamplingFcn       = [];
     end
     
     % Le proprietà a settaggio privato sono:
@@ -65,12 +65,13 @@ classdef Exls3
     %   * gettabili: anche dall'esterno della classe
     %   * settabili: solo dall'interno della classe
     properties (SetAccess = private)
-        ImuData            = [];
-        SamplesAcquired    = 0;
-        ConnectionStatus   = 'closed';
-        AcquisitionStatus  = 'off';
-        PacketsLostNumber  = 0;
-        PacketsLostIndexes = [];
+        ImuData              = [];
+        SamplesAcquired      = 0;
+        ConnectionStatus     = 'closed';
+        AcquisitionStatus    = 'off';
+        PacketsLostNumber    = 0;
+        PacketsLostIndexes   = [];
+        InternalFigureHandle = [];
     end
     
     % Le proprietà nascoste sono:
@@ -78,16 +79,34 @@ classdef Exls3
     %   * gettabili: solo dall'interno della classe
     %   * settabili: solo dall'interno della classe
     properties (Hidden)
-        Bluetooth = [];
-        CreateNewLine = true;
-        Timer = timer('StartFcn',@TimerStartFcn, ...
-            'TimerFcn',@TimerTimerFcn,'StopFcn',@TimerStopFcn);
+        Bluetooth      = [];
+        InternalFigure = true;
+        Timer          = timer('StartFcn',@TimerStartFcn, ...
+            'TimerFcn',@TimerTimerFcn, ...
+            'StopFcn', @TimerStopFcn);
     end
     
     
-    %% PUBLIC METHODS
+    %% METHODS
     %%
+    % spiegazione sui metodi
+    %
+    %
+    %
+    %
+    
     methods (Access = public)
+        %% PUBLIC METHODS
+        %%
+        % spiegazione dei metodi publici
+        %
+        %
+        %
+        %
+        
+        
+        %% EXLS3
+        %%
         function obj = Exls3(name,varargin)
             %EXLS3 Construct EXLs3 object.
             %
@@ -101,48 +120,68 @@ classdef Exls3
             %
             %    See also BLUETOOTH, TIMER.
             
-            %mlock
+            %mlock % i pulisci non sono in grado di pulire la classe
             obj.Name = name;
             
             % validating inputs
             for i = 1:2:numel(varargin)
+                % checking for prop existency
                 if isprop(obj,varargin{i})
+                    % checking for public prop
                     if isaProperty(obj,varargin{i},'SetAccess','public')
+                        % getting PropertyName and PropertyValue
                         PropertyName = varargin{i};
                         PropertyValue = varargin{i+1};
+                        
+                        % switching between cases
                         switch PropertyName
                             case {'Name','Segment'}
+                                % validating Name & Segment
                                 if ischar(PropertyValue)
                                     obj.(PropertyName) = PropertyValue;
                                 else
                                     error([PropertyName,' must be char'])
                                 end
-                            case 'ShowLine'
-                                if PropertyValue == true                   %check for logical
-                                    obj.ShowLine = true;
+                                
+                            case 'FigureVisible'
+                                % validating FigureVisible
+                                if ischar(PropertyValue)
+                                    if any(strcmp(PropertyValue, ...
+                                            {'on','off'}))
+                                        obj.FigureVisible = PropertyValue;
+                                    else
+                                        error(['FigureVisible Proper', ...
+                                            'tyValue must be ''on'' ', ...
+                                            'or ''off'''])
+                                    end
                                 else
-                                    obj.ShowLine = false;
-                                    obj.CreateNewLine = false;
+                                    error(['FigureVisible PropertyVal', ...
+                                        'ue must be a char'])
                                 end
+                                
                             case 'PacketType'
+                                % validating PacketType
                                 mustBeMember(PropertyValue,{'A'})          % mancano valori
                                 obj.PacketType = PropertyValue;
+                                
                             case 'SamplingFrequency'
+                                % validating SamplingFrequency
                                 mustBeNumeric(PropertyValue)
                                 mustBeMember(PropertyValue,[50,100,200])   % mancano valori
                                 obj.SamplingFrequency = PropertyValue;
-                            case 'AnimatedLineHandle'
-                                if isa(PropertyValue, ...
-                                        ['matlab.graphics.', ...
-                                        'animation.AnimatedLine'])
-                                    obj.AnimatedLineHandle = PropertyValue;
-                                    obj.ShowLine = true;
-                                    obj.CreateNewLine = false;
+                                
+                            case 'FigureHandle'
+                                % validating FigureHandle
+                                if isa(PropertyValue,'matlab.ui.Figure')
+                                    obj.FigureHandle = PropertyValue;
+                                    obj.InternalFigure = false;
                                 else
                                     error(['Line handle must be an ', ...
                                         'AnimatedLine'])
                                 end
+                                
                             case {'StopFcn','StartFcn','SamplingFcn'}
+                                % validating Stop, Start & Sampling Fcn
                                 if isa(PropertyValue,'function_handle')
                                     obj.(PropertyName) = PropertyValue;
                                 else
@@ -151,24 +190,52 @@ classdef Exls3
                                 end
                         end
                     else
+                        % The user tried to modify a SetAccess Private prop
                         error(['You cannot set a SetAccess ', ...
                             'private property (',varargin{i},')'])
                     end
                 else
+                    % The user inputed a not valid property
                     error([varargin{i},' is not a valid property'])
                 end
             end
+            
+            % If InternalFigure is true, let's create it
+            if obj.InternalFigure == true
+                obj.InternalFigureHandle = ...
+                    figure('Visible',obj.FigureVisible);
+                c = {'r','b','k'};
+                t = {'Acc';'Gyr';'Mag'};
+                for i = 1:3
+                    ax(i) = subplot(3,1,i);
+                    for j = 1:3
+                        an(i,j) = animatedline(ax(i),'Color',c{1,j});
+                        % other options here
+                    end
+                    title(t{i,1})
+                end
+                linkaxes(ax,'x')
+            end
         end
         
+        
+        %% IMUCONNECT
+        %%
         function obj = imuConnect(obj)
             % qui ci va la grossa routine di connesssione nonché la
             % definizione dei parametri mancanti al timer
         end
         
+        
+        %% IMUSTART
+        %%
         function obj = imuStart(obj)
             start(obj.Timer)
         end
         
+        
+        %% IMUSTOP
+        %%
         function obj = imuStop(obj)
             stop(obj.Timer)
         end
