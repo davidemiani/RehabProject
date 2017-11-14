6519b0b4dc094713e709cda979abeea739af212b
FRANCESCO\francesco <francescomasci1994@gmail.com>
2017-11-14 13:27:23 +0100

    Compute Angles completata, manca differenza fra angoli

---
+++ Frafuncs/computeAngles.m
@@ -0,0 +1,40 @@
+function [Angles] = computeAngles(Exelobj)
+
+switch Exelobj.Segment
+    
+    case 'Thorax'
+        
+        %Txt Sagittal
+        
+        Angles.Thorax.Sagittal.Atan = atan2d(...
+            -Exelobj.ImuData.AccZ , Exelobj.ImuData.AccY)'; %asse z è negativo per avere rotazione in avanti  antioraria positiva;
+        Angles.Thorax.Sagittal.Acos = real(...
+            acosd( round((Exelobj.ImuData.AccZ).* 2 / 2^15, 1)))'-90;
+        
+        %Txt Frontal
+        
+        Angles.Thorax.Frontal.Atan = atan2d(...
+            Exelobj.ImuData.AccX , Exelobj.ImuData.AccY)';
+        Angles.Thorax.Frontal.Acos = real(...
+            acosd( round( -Exelobj.ImuData.AccX .* 2 / 2^15, 1)))'-90;
+        
+    case 'Homer'
+        
+        %Hom Sagittal
+        
+        Angles.Homer.Sagittal.Atan =  atan2d(...
+            Exelobj.ImuData.AccX , Exelobj.ImuData.AccY)';
+        Angles.Homer.Sagittal.Acos = real(...
+            acosd( round( -Exelobj.ImuData.AccX.* 2 / 2^15, 1)))'-90;
+        
+        %Hom frontal
+        
+        Angles.Homer.Frontal.Atan= atan2d(...
+            Exelobj.ImuData.AccZ, Exelobj.ImuData.AccY)';
+        Angles.Homer.Frontal.Acos= real(...
+            acosd( round( - Exelobj.ImuData.AccZ.* 2 / 2^15, 1)))'-90;%
+        
+end
+
+end
+
