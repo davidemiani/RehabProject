fa962ce19b13d013d52bc20c5e81218c5086aa0f
ASUSFRANCO\Franco <franco.pradelli@studio.unibo.it>
2017-11-24 21:37:24 +0100

    Grafici completi di valutazione del calcolo degli angoli attraverso atan e acos su manichino. Singolo sensore sull'omero.

--- Acquisizioni/21-11-2017/mainValutazionePrecisioneAngoli.m
+++ Acquisizioni/21-11-2017/mainValutazionePrecisioneAngoli.m
@@ -39,7 +39,7 @@ for j = 1:nAngoli
     computeFrontalAngle(datiPerAngolo(:,5),datiPerAngolo(:,6));
 end
 
-% Plottiamo
+% Plottiamo i risultati di acos e atan rispetto al valore ideale
 h = figure;
 legin = {'valore ideale', 'acos', 'atan'};
 for j = 1:nAngoli
@@ -53,9 +53,10 @@ for j = 1:nAngoli
     title([angoli{j} ' gradi'])
     legend(legin)
 end
+set(h, 'Name', 'Piano Frontale')
 
 % Salviamo i plot
-savefig2(h, fullfile(pwd, 'grafici', 'confrontoValutazioneAngoli.fig'), 'ScreenSize', true)
+savefig2(h, fullfile(pwd, 'grafici', 'confrontoValutazioneAngoliFront.fig'), 'ScreenSize', false)
 
 % Valutiamo media, varianza e differenza
 mediaValutazioni = struct('atan', zeros(nAngoli, 1), 'acos', zeros(nAngoli, 1));
@@ -88,6 +89,102 @@ for i = 1:3
     grid minor
     legend(legin)
 end
+set(h2, 'Name', 'Piano Frontale')
 
 % Salviamo i plot
-savefig2(h2, fullfile(pwd, 'grafici', 'indiciValutazioniAngoli.fig'), 'ScreenSize', true)
+savefig2(h2, fullfile(pwd, 'grafici', 'indiciValutazioniAngoliFront.fig'), 'ScreenSize', false)
+
+%% Piano sagittale
+
+% Carico i dati
+datiSag = struct('deg0', [], 'deg45', [], 'deg90', [], 'deg135', []);
+angoli = {'0', '45', '90', '135'};
+nAngoli = length(angoli);
+piano = 'S';
+nAcqu = 5;
+struttura = load('0_1S');
+% carico 0 separatamente (causa dati salvati in maniera differente)
+datiSag.deg0 = struttura.dataHum;
+struttura = load('0_2S');
+datiSag.deg0 = [datiSag.deg0; struttura.dataHum];
+struttura = load('0_345S');
+datiSag.deg0 = [datiSag.deg0; struttura.dataHum];
+% carico angoli da 45 a 135
+for j = 2:nAngoli 
+    nomeFile = [angoli{1, j}, '_12345', piano];
+    struttura = load(nomeFile);
+    datiSag.(['deg' angoli{j}]) = struttura.dataHum;
+end
+
+% Calcolo gli angoli (algoritmo tutor)
+angoliCalcolatiSag = struct('deg0', struct('atan', [], 'acos', []), ...
+    'deg45', struct('atan', [], 'acos', []), ...
+    'deg90', struct('atan', [], 'acos', []), ...
+    'deg135', struct('atan', [], 'acos', []));
+for j = 1:nAngoli
+    campoAngolo = ['deg' angoli{j}];
+    if j == nAngoli
+        datiPerAngolo = datiSag.(campoAngolo); % a 135 i dati sono già a normalizzati a g
+    else 
+        datiPerAngolo = datiSag.(campoAngolo) * 2 * 9.807 / 32768;
+    end
+    [angoliCalcolatiSag.(campoAngolo).acos, ...
+    angoliCalcolatiSag.(campoAngolo).atan] = ...
+    computeSagittalAngle(datiPerAngolo(:,4),datiPerAngolo(:,5));
+end
+
+% Plottiamo
+h = figure;
+legin = {'valore ideale', 'acos', 'atan'};
+for j = 1:nAngoli
+    campoAngolo = ['deg' angoli{j}];
+    subplot(2, 2, j)
+    plot(ones(size(angoliCalcolatiSag.(campoAngolo).acos))*str2double(angoli{j}), ...
+        'LineWidth', 2) % plotto valore ideale
+    hold on
+    plot(angoliCalcolatiSag.(campoAngolo).acos) % plotto acos
+    plot(angoliCalcolatiSag.(campoAngolo).atan) % plotto atan
+    hold off
+    title([angoli{j} ' gradi'])
+    legend(legin)
+end
+set(h, 'Name', 'Piano Sagittale')
+
+% Salviamo i plot
+savefig2(h, fullfile(pwd, 'grafici', 'confrontoValutazioneAngoliSag.fig'), 'ScreenSize', false)
+
+% Valutiamo media, varianza e differenza
+mediaValutazioni = struct('atan', zeros(nAngoli, 1), 'acos', zeros(nAngoli, 1));
+stdvarValutazioni = struct('atan', zeros(nAngoli, 1), 'acos', zeros(nAngoli, 1));
+diffValutazioni = struct('atan', zeros(nAngoli, 1), 'acos', zeros(nAngoli, 1));
+for j = 1:nAngoli
+    campoAngolo = ['deg' angoli{j}];
+    mediaValutazioni.atan(j) = mean(angoliCalcolatiSag.(campoAngolo).atan); % salvo media per di atan per ogni angolo
+    mediaValutazioni.acos(j) = mean(angoliCalcolatiSag.(campoAngolo).acos); % salvo media per di acos per ogni angolo
+    stdvarValutazioni.atan(j) = std(angoliCalcolatiSag.(campoAngolo).atan); % salvo std per di atan per ogni angolo
+    stdvarValutazioni.acos(j) = std(angoliCalcolatiSag.(campoAngolo).acos); % salvo std per di acos per ogni angolo
+    diffValutazioni.atan(j) = mean(angoliCalcolatiSag.(campoAngolo).atan) - str2double(angoli{j}); % salvo std per di atan per ogni angolo
+    diffValutazioni.acos(j) = mean(angoliCalcolatiSag.(campoAngolo).acos) - str2double(angoli{j}); % salvo std per di acos per ogni angolo
+end
+valutazioniIndici.mediaValutazioni = mediaValutazioni;
+valutazioniIndici.stdvarValutazioni = stdvarValutazioni;
+valutazioniIndici.diffValutazioni = diffValutazioni;
+
+angolid = str2double(angoli); % definisco gli angoli in double
+legin = {'acos', 'atan'};
+h2 = figure;
+indice = {'media', 'deviazione standard', 'differenza media dal valore vero'};
+indiceBreve = {'media', 'stdvar', 'diff'};
+for i = 1:3
+    subplot(1, 3, i)
+    plot(angolid, valutazioniIndici.([indiceBreve{i} 'Valutazioni']).acos, '-o', ...
+        angolid, valutazioniIndici.([indiceBreve{i} 'Valutazioni']).atan, '-o')
+    title(indice{i})
+    xlabel('angoli')
+    grid minor
+    legend(legin)
+end
+set(h2, 'Name', 'Piano Sagittale')
+
+% Salviamo i plot
+savefig2(h2, fullfile(pwd, 'grafici', 'indiciValutazioniAngoliSag.fig'), 'ScreenSize', false)
