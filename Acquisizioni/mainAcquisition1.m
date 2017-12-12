26655358e9ca7230cff7934f5019595ad20eb519
davidemiani <davide.miani2@gmail.com>
2017-12-12 13:42:39 +0100

    Riaggiunto il primo main per singolo sensore.

---
+++ Acquisizioni/mainAcquisition1.m
@@ -0,0 +1,71 @@
+% mainAcquisituon.m
+
+
+%% INIT
+%%
+% getting current script directory
+csd = fileparts(mfilename('fullpath'));
+
+% computing repository main dir
+repopath = fileparts(csd);
+
+% computing data dir
+datapath = fullfile(repopath,'Acquisizioni', ...
+    char(datetime('now','Format','yyyy-MM-dd')));
+
+% assuring you have the right function paths
+addpath(fullfile(repopath,'Exel_class'))
+addpath(fullfile(repopath,'davefuncs'))
+addpath(fullfile(csd,'davetest'))
+
+% clearing old vars
+clc, close all, clearExcept csd datapath
+
+% checking if the datapath is existent. If it doesn't, touch will create it
+touch(datapath);
+
+% setting ExelName and Fig
+ExelName = 'EXLs3_0070';
+TestDuration = 10;
+ExelFigure = testFigure1();
+
+% creating the object
+obj = Exel(ExelName,'ExelFigure',ExelFigure,'SamplingFcn',@testFunc);
+set(obj,'AutoStop',TestDuration)
+
+% starting it, if error, exiting
+try
+    start(obj)
+catch ME
+    disconnect(obj)
+    rethrow(ME)
+end
+
+
+%% SAVING DATA
+%%
+% waiting for the acquisition stop
+waitfor(obj,'AcquisitionStatus','off')
+
+% picking data to save
+dataHum = [repmat(32,height(obj.ExelData),1), ...
+    obj.ExelData.PacketType, ...
+    obj.ExelData.ProgrNum, ...
+    obj.ExelData.AccX, ...
+    obj.ExelData.AccY, ...
+    obj.ExelData.AccZ, ...
+    floor(255*rand(height(obj.ExelData),1))]; 
+
+% jumping to data directory
+cd(datapath)
+
+% asking the user for saving
+uisave('dataHum','test_name')
+
+% coming back to the current script directory
+cd(csd)
+
+% if no file was saved
+if isempty(files2cell(datapath))
+    rmdir(datapath)
+end
