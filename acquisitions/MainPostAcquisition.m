c7a2c32fbbdfda7844363fc060b3633a82b43c15
MARTA4\9marta4 <marta.costantini4@gmail.com>
2018-01-23 12:58:58 +0100

    prima validazione mainPostAcquisition

--- acquisitions/MainPostAcquisition.m
+++ acquisitions/MainPostAcquisition.m
@@ -3,12 +3,14 @@
 pulisci
 %% loading 
 acqdir = cd;
-cd(fullfile(cd,'2018-01-18'));  %data directory
-load('MM17_09.mat');            %data file
+cd(fullfile(cd,'2017-12-05'));  %data directory
+load('DM94_20.mat');            %data file
 cd(acqdir);
 
 Nobjs = numel(obj); %2
 Ts = 1/obj(1,1).SamplingFrequency;
+
+objold = obj;
 
 %% sorting
 for i = 1:Nobjs
@@ -32,129 +34,135 @@ end
 
 
 %% synchronization 
-
-for i = 1:Nobjs
-    %seconds of each datetime considering the time lag between start function and the first packet,too 
-    DataStart(i,1) = second(obj(i,1).StartTime) + Ts*obj(i,1).ExelData.ProgrNum(1);
-end
-
-%getting the last in time obj
-[~,lastObjStart] = max(DataStart);
-
-%getting time beetween each DataStart and the last one
-timeToLastObj    = abs(DataStart-DataStart(lastObjStart));
-
-%computing samples difference 
-samplesToLastObj = floor(timeToLastObj/Ts);
-
-if nnz(samplesToLastObj)
+if Nobjs>1
     for i = 1:Nobjs
-        obj(i,1).ExelData = obj(i,1).ExelData(samplesToLastObj(i)+1:end,:);
-        obj(i,1).ExelData.ProgrNum = ...
-            obj(i,1).ExelData.ProgrNum - samplesToLastObj(i);
+        %seconds of each datetime considering the time lag between start function and the first packet,too
+        DataStart(i,1) = second(obj(i,1).StartTime) + Ts*obj(i,1).ExelData.ProgrNum(1);
+    end
+    
+    %getting the last in time obj
+    [~,lastObjStart] = max(DataStart);
+    
+    %getting time beetween each DataStart and the last one
+    timeToLastObj    = abs(DataStart-DataStart(lastObjStart));
+    
+    %computing samples difference
+    samplesToLastObj = floor(timeToLastObj/Ts);
+    
+    if nnz(samplesToLastObj)
+        for i = 1:Nobjs
+            obj(i,1).ExelData = obj(i,1).ExelData(samplesToLastObj(i)+1:end,:);
+            obj(i,1).ExelData.ProgrNum = ...
+                obj(i,1).ExelData.ProgrNum - samplesToLastObj(i);
+        end
     end
 end
-
-
+ %% final checking
+% for i = 1:Nobjs
+%     MissingPacketsReport(i,1) = CheckMissPack(obj(i,1));
+% end
+% %pause
 
 function vProgr = ProgrNumSorting(v)
 %ordinamento del vettore ProgrNum di ExelData eliminando i limiti 0-10000
-M = 10000;
+M = 9999;
 
 %trovo le posizioni in cui ricomincia la numerazione oppure c'è un salto
-dneg = find(diff(v)~=1);
+dneg = find(diff(v)~=1)';
 
-if isempty(dneg)
-    vProgr = v;
-    return
-end
-
-v = v';
-%divido il vettore v in intervalli aventi numerazione consecutiva
-vcell = mat2cell(v,1,[dneg(1) diff(dneg) length(v)-dneg(end)]);
-
-n_int = numel(vcell);
-first = vcell{1}(1);
-
-%nuovo vettore riordinato
-vprogr = first:vcell{1}(end);
-
-%nel for vengono analizzati tutti i casi di passaggio da un intervallo
-%all'altro
-
-for i = 2:n_int
-    Ncell = numel(vcell{i});
+if ~isempty(dneg) 
+    v = v';
+    %divido il vettore v in intervalli aventi numerazione consecutiva
+    vcell = mat2cell(v,1,[dneg(1) diff(dneg) length(v)-dneg(end)]);
     
-    if vcell{i-1}(end) == M & vcell{i}(1) == 0
-        Miss = 0;
+    n_int = numel(vcell);
+    first = vcell{1}(1);
+    
+    %nuovo vettore riordinato
+    vProgr = first:vcell{1}(end);
+    
+    %nel for vengono analizzati tutti i casi di passaggio da un intervallo
+    %all'altro
+    
+    for i = 2:n_int
+        Ncell = numel(vcell{i});
         
-    elseif vcell{i-1}(end) == M & vcell{i}(1) > 0
-        Miss = vcell{i}(1);
+        if vcell{i-1}(end) == M & vcell{i}(1) == 0
+            Miss = 0;
+            
+        elseif vcell{i-1}(end) == M & vcell{i}(1) > 0
+            Miss = vcell{i}(1);
+            
+        elseif vcell{i-1}(end) < M & vcell{i}(1) == 0
+            Miss = M-vcell{i-1}(end);
+            
+        elseif vcell{i-1}(end) < M & vcell{i}(1) > 0 & vcell{i-1}(end) > vcell{i}(1)
+            Miss = vcell{i}(1) + M-vcell{i-1}(end);
+            
+        elseif vcell{i-1}(end) < vcell{i}(1)
+            Miss = vcell{i}(1) - vcell{i-1}(end) - 1;
+            
+        end
         
-    elseif vcell{i-1}(end) < M & vcell{i}(1) == 0
-        Miss = M-vcell{i-1}(end);
- 
-    elseif vcell{i-1}(end) < M & vcell{i}(1) > 0 & vcell{i-1}(end) > vcell{i}(1)
-        Miss = vcell{i}(1) + M-vcell{i-1}(end);
-        
-    elseif vcell{i-1}(end) < vcell{i}(1) 
-        Miss = vcell{i}(1) - vcell{i-1}(end) - 1
-        
+        vProgr = [vProgr vProgr(end) + Miss + 1 : vProgr(end) + Ncell + Miss];
     end
-    
-    vProgr = [vprogr vprogr(end) + Miss + 1 : vprogr(end) + Ncell + Miss];
-
+    vProgr = vProgr';
+else  %il vettore ProgrNum ha tutti numeri consecutivi
+    vProgr = v;
 end
-vProgr = vProgr';
 end
 function ExelData = AddMissRow(MissingPacketsReport,ExelData,MaxforInterp,PacketName)
 
 for int = 1:size(MissingPacketsReport.WhereMiss)
-    
+    int
     if istable(MissingPacketsReport.WhereMiss)
-        lastSampleIndex = MissingPacketsReport.WhereMiss.CutPointIndex(int);
+        lastProgrNum = MissingPacketsReport.WhereMiss.CutProgrNum(int);
         deltaSample = MissingPacketsReport.WhereMiss.SamplesNum(int);
     else
-        lastSampleIndex = MissingPacketsReport.WhereMiss;
+        lastProgrNum = MissingPacketsReport.WhereMiss;
         deltaSample = MissingPacketsReport.MissSamplesTot;
     end
     
     
     %ampliamento tabella dati con zeri
-    ExelData{ lastSampleIndex+1 : end+deltaSample, :} =...
+    ExelData{ find( ExelData.ProgrNum == lastProgrNum) + 1 : end+deltaSample, :} = ...
         [ zeros(deltaSample,16);
-        ExelData{lastSampleIndex+1 : end, :}];
+        ExelData{ find( ExelData.ProgrNum == lastProgrNum) + 1 : end, :}];
+        
+%     ExelData{ lastProgrNum+1 : end+deltaSample, :} =...
+%         [ zeros(deltaSample,16);
+%         ExelData{lastProgrNum+1 : end, :}];
     
     
     %reimposto la colonna ProgrNum inserendo numeri progressivi
     ExelData.ProgrNum(...
-        lastSampleIndex+1 : lastSampleIndex + 1 + deltaSample) = ...
-        (lastSampleIndex : lastSampleIndex+deltaSample)';
+        lastProgrNum + 2 : lastProgrNum + deltaSample + 1) = ...
+        (lastProgrNum + 1 : lastProgrNum + deltaSample)';
     
     
     %reimposto la colonna PacketType e Vbat inserendo l'ultimo
     %valore prima della perdita
     ExelData{...
-        lastSampleIndex+1 : lastSampleIndex + deltaSample,[2 end]} = ...
-        ExelData{lastSampleIndex,[2 end]}.*ones(deltaSample,2);
+        lastProgrNum + 2 : lastProgrNum + deltaSample + 1,[2 end]} = ...
+        ExelData{lastProgrNum + 1,[2 end]}.*ones(deltaSample,2);
     
     
     if deltaSample > MaxforInterp
             
         ExelData{...
-            lastSampleIndex+1 : lastSampleIndex + deltaSample,3:15} = ...
+            lastProgrNum+1 : lastProgrNum + deltaSample,3:15} = ...
             nan.*ones(deltaSample,13);
         
     else
         
-        ExelData = InterpMissData(ExelData,lastSampleIndex,deltaSample,PacketName);
+        ExelData = InterpMissData(ExelData,lastProgrNum,deltaSample,PacketName);
         
     end
 end
 end
-function ExelData = InterpMissData(ExelData,lastSampleIndex,deltaSample,PacketName)
+function ExelData = InterpMissData(ExelData,lastProgrNum,deltaSample,PacketName)
 
-x = ExelData{[lastSampleIndex lastSampleIndex+deltaSample+1],1};
+x = [lastProgrNum lastProgrNum + deltaSample + 1];
 GrInterp=1;
 if  PacketName == 'A'
     lastcol = 5;
@@ -164,13 +172,13 @@ end
 
 for column = 3:lastcol
     
-    y = ExelData{[lastSampleIndex lastSampleIndex+deltaSample+1],column};
+    y = ExelData{[lastProgrNum + 1 lastProgrNum + 1 + deltaSample + 1],column}';
     coeff = polyfit(x,y,GrInterp);
     xnew=x(1):x(2);
-
+    ynew=polyval(coeff,xnew)';
     ExelData{...
-        lastSampleIndex : lastSampleIndex + deltaSample + 1,column} = ...
-        polyval(coeff,xnew)';
+        lastProgrNum + 1 : lastProgrNum + 1 + deltaSample + 1,column} = ...
+        ynew;
 end
 end
 function MissingPackets = CheckMissPack(obj)
@@ -209,12 +217,12 @@ if missint>0
     fprintf('\nOps! Some packets miss!\n\n');
     
     if missint == 1
-        MissingPackets.WhereMiss = find(DeltaSamples>1);
+        MissingPackets.WhereMiss = obj.ExelData.ProgrNum(DeltaSamples>1);
     else
         MissingPackets.WhereMiss = table;
-        MissingPackets.WhereMiss.CutPointIndex = find(DeltaSamples>1); %se find=2 vuol dire che tra il campione 2 e 3 ho perso pacchetti
-        MissingPackets.WhereMiss.SamplesNum    = DeltaSamples(MissingPackets.WhereMiss.CutPointIndex)-1;
-        MissingPackets.WhereMiss.Time          = MissingPackets.WhereMiss.SamplesNum/fs;
+        MissingPackets.WhereMiss.CutProgrNum  = obj.ExelData.ProgrNum(DeltaSamples>1); %se find=2 vuol dire che tra il campione 2 e 3 ho perso pacchetti
+        MissingPackets.WhereMiss.SamplesNum    = DeltaSamples(DeltaSamples>1)-1;
+        MissingPackets.WhereMiss.Time          = MissingPackets.WhereMiss.SamplesNum/obj.SamplingFrequency;
     end
     
     MissingPackets.ismiss = true;
